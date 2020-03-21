7a331a86f23af048b752aa65aa30072b208976db
SlamPig <devanf1991@gmail.com>
2020-03-20 20:36:51 -0700

    removed un-needed lines

--- Assignments/Devan/1 Python/labs/lab08-make_change.py
+++ Assignments/Devan/1 Python/labs/lab08-make_change.py
@@ -1,9 +1,4 @@
-import random
 
-penny = 1
-nickle = 5
-dime = 10
-quarter = 25
-dollar = 100
 num_of_pennies = int(input('How many pennies do you have? '))
+
 print(f'I can give you {num_of_pennies // 25} quarters and {num_of_pennies % 25} pennies.')
