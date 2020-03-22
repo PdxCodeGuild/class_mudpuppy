1459629954a2cad2906b18cad79d19afaa89b415
Eboni Washington <eboni.washington822@gmail.com>
2020-03-22 01:22:15 -0700

    adding lab1

---
+++ Assignments/Eboni/lab1.py
@@ -0,0 +1,23 @@
+# Hello Class
+"""
+    Lab 1
+    by: Eboni
+"""
+
+# print ("what up g")
+# Something here
+
+# Something there
+
+"""
+Now I can type
+Whatever I want and blah
+blah blah blah
+"""
+
+
+message = "Hello "
+name = input ("Enter your name:  ")
+# print (message + name)
+
+print (f"{message}to {name}")
