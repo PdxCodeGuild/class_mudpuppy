c9fbe6c1a4da7808f69730e8f02959ce611b7a02
SlamPig <devanf1991@gmail.com>
2020-03-22 14:38:48 -0700

    Made version 2 of random emoticon generator

--- Assignments/Devan/1 Python/labs/lab08-make_change_v1a.py
+++ Assignments/Devan/1 Python/labs/lab08-make_change_v1a.py
@@ -1,2 +1,4 @@
-
-dollar_amount = int(input('How much money do you have (in decimal format)? '))
+dollarAmount = input('How much money do you have (in decimal format)? ')
+dollarAmount = float(dollarAmount)
+dollarAmount = round(dollarAmount * 100)
+print(dollarAmount)
