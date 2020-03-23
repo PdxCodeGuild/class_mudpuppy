19e2ccdc6e2b1e2af7f8fba31d5033bf9f94bde0
Devan Fischer <devanf1991@gmail.com>
2020-03-22 20:13:57 -0700

    updated make change advance version 1

--- Assignments/Devan/1 Python/labs/lab08-make_change_v1a.py
+++ Assignments/Devan/1 Python/labs/lab08-make_change_v1a.py
@@ -1,4 +1,9 @@
-dollarAmount = input('How much money do you have (in decimal format)? ')
-dollarAmount = float(dollarAmount)
-dollarAmount = round(dollarAmount * 100)
-print(dollarAmount)
+user_amount = float(input('How much money do you have (ex. 1.25)? '))
+user_amount = round(user_amount * 100)
+dollars = user_amount // 100
+quarters = user_amount % 100 // 25
+dimes = user_amount % 100 % 25 // 10
+nickles = user_amount % 100 % 25 % 10 // 5
+pennies = user_amount % 5
+
+print(f'I can give you {dollars} dollars, {quarters} quarters, {dimes} dimes, {nickles} nickles, and {pennies} pennies.')
