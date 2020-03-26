c1b43684deceb7f8a027a6730e276268b3b5b6d8
Your Name Here <your_email@youremail.com>
2020-03-25 19:01:56 -0700

    adding Lab 11 v1

---
+++ Assignments/Brea/simple_calculator_lab.py
@@ -0,0 +1,23 @@
+#simple calculator lab 3.25.20
+
+op_input = input("What operation would you like to perform? (+, -, *, /) : ")
+
+num1 = input("What is the first number? : ")
+
+num2 = input("What is the second number? : ")
+
+if op_input == "+" :
+    add_sum = float(num1) + float(num2)
+    print(f"{num1} + {num2}  =  {add_sum}")
+    
+if op_input == "-" :
+    sub_sum = float(num1) - float(num2)
+    print(f"{num1} - {num2}  =  {sub_sum}")
+
+if op_input == "*" :
+    prod = float(num1) * float(num2)
+    print(f"{num1} * {num2}  =  {prod}")
+
+if op_input == "/" :
+    div = float(num1) / float(num2)
+    print(f"{num1} / {num2}  =  {div}")
