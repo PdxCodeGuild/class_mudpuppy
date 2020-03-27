73b40035296566eabb9412e3ddbc92e887bc921a
evanhackett <evanwhackett@gmail.com>
2020-03-26 21:12:29 -0700

    added in-class demo of binary search

---
+++ 1 Python/class_demos/binary-search/guess_number.py
@@ -0,0 +1,20 @@
+
+def average(x,y):
+    return (x + y) // 2
+
+def search(lower, upper, target, guess):
+    print(f'You guessed: {guess}')
+
+    if guess == target:
+        print('Correct!')
+    elif guess < target:
+        print('Too low')
+        search(guess, upper, target, average(guess, upper)+1)
+    elif guess > target:
+        print('Too high')
+        search(lower, guess, target, average(lower, guess))
+
+
+
+
+search(1,100,13,1)
