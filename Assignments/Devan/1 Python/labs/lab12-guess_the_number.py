e99604b1869e2d1fa2f9bf568e009cf32148d2a7
Devan Fischer <devanf1991@gmail.com>
2020-03-26 21:27:04 -0700

    finished v4 of guess the number

--- Assignments/Devan/1 Python/labs/lab12-guess_the_number.py
+++ Assignments/Devan/1 Python/labs/lab12-guess_the_number.py
@@ -1,17 +1,41 @@
 import random
 
-comp_num = random.randint(1, 10)
-
+comp_num = 5#random.randint(1, 10)
 guess_count = 0
+last_guess = None
+
+
+def find_avg(comp_num, usr_num):
+    return abs(comp_num - usr_num)
+
 
 while True:
-    usr_num = int(input('Guess a number between 1 and 10: '))
+    usr_num = int(input('\nGuess a number between 1 and 10: '))
     if usr_num == comp_num:
         print(f'\nCorrect, the number was {comp_num}. You guessed it in {guess_count} guesses.')
         break
-    elif usr_num > comp_num:
-        guess_count += 1
-        print(f'\nToo high. ')
-    elif usr_num < comp_num:
-        guess_count += 1
-        print(f'\nToo low. ')
+
+    print('\nSorry you\'re wrong.')
+
+    # elif usr_num > comp_num:
+    #     guess_count += 1
+    #     print(f'\nToo high. ')
+    # elif usr_num < comp_num:
+    #     guess_count += 1
+    #     print(f'\nToo low. ')
+
+    if last_guess is not None:
+        a_guess = find_avg(comp_num, usr_num)
+        a_last_guess = find_avg(comp_num, last_guess)
+        if a_guess < a_last_guess:
+            print('You\'re getting warmer.')
+        elif a_guess > a_last_guess:
+            print('You\'re getting colder.')
+        elif a_guess == a_last_guess:
+            print('You entered the same number....')
+
+    print('\nYou guessed: ' + str(usr_num))
+    print('Your last guess was: ' + str(last_guess))
+
+    last_guess = usr_num
+    guess_count += 1
