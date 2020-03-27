4b40a89e4ba1e618b59b23adbbad6f6d7b3e1ccf
Kadir <Kadir232@live.com>
2020-03-26 21:12:17 -0700

    adding my labs pdx

--- Assignments/Kadir/Lab_10_AverageNumbers.py
+++ Assignments/Kadir/Lab_10_AverageNumbers.py
@@ -1,22 +1,8 @@
 #Lab10.py
 n = int(input("Enter how many numbers you wish to average: "))
-
-
-
 a = []
-
-
-
-for i 
-in range(0,n):
-
-elem=int(input("Enter number: "))
-
-a.append(elem)
-
+for i in range(0,n):
+    elem=int(input("Enter number: "))
+    a.append(elem)
 avg = sum(a)/n
-
-
-
-print("Average of numbers in the list",round(avg,
-2))
+print("Average of numbers in the list",round(avg, 2))
