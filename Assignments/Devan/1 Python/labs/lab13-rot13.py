d430ed646dcb8466b51eb18b9ba65949959d5d46
Devan Fischer <devanf1991@gmail.com>
2020-03-27 20:41:01 -0700

    added support for punctuation, spaces, and capitals

--- Assignments/Devan/1 Python/labs/lab13-rot13.py
+++ Assignments/Devan/1 Python/labs/lab13-rot13.py
@@ -1,18 +1,37 @@
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
-alphabet_rotated = 'nopqrstuvwxyzabcdefghijklm'
+import string as str
+
 
 def encrypt(text):
     encryption = ''
     for letter in text:
-        i = alphabet.find(letter)
-        encryption += alphabet_rotated[i]
+        if letter in str.punctuation:
+            encryption += letter
+        if letter in str.whitespace:
+            encryption += letter
+        if letter in str.ascii_lowercase:
+            i = str.ascii_lowercase.find(letter) - 13
+            encryption += str.ascii_lowercase[i]
+        if letter in str.ascii_uppercase:
+            i = str.ascii_uppercase.find(letter) - 13
+            encryption += str.ascii_uppercase[i]
     return encryption
 
 def decrypt(text):
     decryption = ''
     for letter in text:
-        i = alphabet_rotated.find(letter)
-        decryption += alphabet[i]
+        if letter in str.punctuation:
+            decryption += letter
+        if letter in str.whitespace:
+            decryption += letter
+        if letter in str.ascii_lowercase:
+            i = (str.ascii_lowercase.find(letter) + 13) % len(str.ascii_lowercase)
+            decryption += str.ascii_lowercase[i]
+        if letter in str.ascii_uppercase:
+            i = (str.ascii_uppercase.find(letter) + 13) % len(str.ascii_uppercase)
+            decryption += str.ascii_uppercase[i]
     return decryption
 
-print(encrypt(input('What would you like to encrypt? ')))
+# print(decrypt("Mrffntr zr lbhe svefg naq ynfg anzr, naq jung glcr bs jbex lbh ubcr gb qb jura lbh tenqhngr. Ayfb fraq zr n cnentencu funevat ubj lbh orpnzr vagrerfgrq va pbzchgre cebtenzzvat."))
+
+print(encrypt('Hello. My name is Devan. '))
+print(decrypt('Uryyb. Zl anzr vf Qrina.'))
