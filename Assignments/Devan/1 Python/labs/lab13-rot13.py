329d64b3e126df1f6be9f654ccfb71c1f39e177b
Devan Fischer <devanf1991@gmail.com>
2020-03-28 16:14:00 -0700

    refactored, added clipboard copy, menu, and colored output

--- Assignments/Devan/1 Python/labs/lab13-rot13.py
+++ Assignments/Devan/1 Python/labs/lab13-rot13.py
@@ -1,37 +1,60 @@
 import string as str
+import clipboard
+from colorama import Fore, Style
+
+class Rot13:
+
+    def menu(self):
+        action = input("\nEnter encrypt, decrypt, or done: ")
+        if action == 'encrypt':
+            rot13.encrypt()
+        if action == 'decrypt':
+            rot13.decrypt()
+        if action == 'done':
+            pass
+        else:
+            print('\nNot a valid input. ')
+            rot13.menu()
+
+    def encrypt(self):
+        text = input('What would you like to encrypt: ')
+        encryption = ''
+        for letter in text:
+            if letter in str.punctuation or letter in str.whitespace:
+                encryption += letter
+            if letter in str.ascii_lowercase:
+                i = str.ascii_lowercase.find(letter) - 13
+                encryption += str.ascii_lowercase[i]
+            if letter in str.ascii_uppercase:
+                i = str.ascii_uppercase.find(letter) - 13
+                encryption += str.ascii_uppercase[i]
+        print('\nYour encrypted text is: ' + Fore.LIGHTGREEN_EX + encryption)
+        print(Style.RESET_ALL)
+        print('\nThe text has been copied to your clipboard')
+        clipboard.copy(encryption)
+        rot13.menu()
+
+    def decrypt(self):
+        text = input('What would you like to decrypt: ')
+        decryption = ''
+        for letter in text:
+            if letter in str.punctuation or letter in str.whitespace:
+                decryption += letter
+            if letter in str.ascii_lowercase:
+                i = (str.ascii_lowercase.find(letter) + 13) % len(str.ascii_lowercase)
+                decryption += str.ascii_lowercase[i]
+            if letter in str.ascii_uppercase:
+                i = (str.ascii_uppercase.find(letter) + 13) % len(str.ascii_uppercase)
+                decryption += str.ascii_uppercase[i]
+        print('\nYour decrypted text is: ' + Fore.LIGHTGREEN_EX + decryption)
+        print(Style.RESET_ALL)
+        print('\nThe text has been copied to your clipboard')
+        clipboard.copy(decryption)
+        rot13.menu()
 
 
-def encrypt(text):
-    encryption = ''
-    for letter in text:
-        if letter in str.punctuation:
-            encryption += letter
-        if letter in str.whitespace:
-            encryption += letter
-        if letter in str.ascii_lowercase:
-            i = str.ascii_lowercase.find(letter) - 13
-            encryption += str.ascii_lowercase[i]
-        if letter in str.ascii_uppercase:
-            i = str.ascii_uppercase.find(letter) - 13
-            encryption += str.ascii_uppercase[i]
-    return encryption
+rot13 = Rot13()
 
-def decrypt(text):
-    decryption = ''
-    for letter in text:
-        if letter in str.punctuation:
-            decryption += letter
-        if letter in str.whitespace:
-            decryption += letter
-        if letter in str.ascii_lowercase:
-            i = (str.ascii_lowercase.find(letter) + 13) % len(str.ascii_lowercase)
-            decryption += str.ascii_lowercase[i]
-        if letter in str.ascii_uppercase:
-            i = (str.ascii_uppercase.find(letter) + 13) % len(str.ascii_uppercase)
-            decryption += str.ascii_uppercase[i]
-    return decryption
+rot13.menu()
 
-# print(decrypt("Mrffntr zr lbhe svefg naq ynfg anzr, naq jung glcr bs jbex lbh ubcr gb qb jura lbh tenqhngr. Ayfb fraq zr n cnentencu funevat ubj lbh orpnzr vagrerfgrq va pbzchgre cebtenzzvat."))
-
-print(encrypt('Hello. My name is Devan. '))
-print(decrypt('Uryyb. Zl anzr vf Qrina.'))
+print("Thank's for using the cypher.")
