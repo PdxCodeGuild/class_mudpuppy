b7747cf1d63abb2a8afe261216028a040cc8742d
Devan Fischer <devanf1991@gmail.com>
2020-03-31 19:51:37 -0700

    refactored

--- Assignments/Devan/1 Python/labs/lab15-number_to_phrase.py
+++ Assignments/Devan/1 Python/labs/lab15-number_to_phrase.py
@@ -1,22 +1,31 @@
 
 
-singles_and_teens = {1: 'One', 2: 'Two', 3: 'Three', 4: 'Four', 5: 'Five', 6: 'Six', \
-                7: 'Seven', 8: 'Eight', 9: 'Nine', 10: 'Ten', 11: 'Eleven', \
-                12: 'Twelve', 13: 'Thirteen', 14: 'Fourteen', 15: 'Fifteen', \
-                16: 'Sixteen', 17: 'Seventeen', 18: 'Eighteen', 19: 'Nineteen'}
-
-double_digit = ['Twenty', 'Thirty', 'Forty', 'Fifty', 'Sixty', 'Seventy', 'Eighty', 'Ninety']
-
-
-user_input = int(input("Please enter a number: "))
-hundreds_digit = user_input // 100
-tens_digit = (user_input % 100)//10
-ones_digit = user_input % 10
+# Define our dicts of numbers in text form
+singles = {1: 'One', 2: 'Two', 3: 'Three', 4: 'Four', 5: 'Five', 6: 'Six', \
+           7: 'Seven', 8: 'Eight', 9: 'Nine'}
+teens = {10: 'Ten', 11: 'Eleven', 12: 'Twelve', 13: 'Thirteen', 14: 'Fourteen', \
+         15: 'Fifteen', 16: 'Sixteen', 17: 'Seventeen', 18: 'Eighteen', 19: 'Nineteen'}
+tens = ['Twenty', 'Thirty', 'Forty', 'Fifty', 'Sixty', 'Seventy', 'Eighty', 'Ninety']
 
 
-if user_input < 20:
-    print(f"Your number is: {singles_and_teens[user_input]}")
-elif user_input > 19 < 100:
-    print(f"Your number is: {double_digit[tens_digit-2]} {singles_and_teens[ones_digit]}")
-elif user_input > 99 < 1000:
-    print(f"Your number is: {singles_and_teens[hundreds_digit]} hundred {double_digit[tens_digit-2]} {singles_and_teens[ones_digit]}")
+def tens_text(num):
+    """Converts an number less than 100 to text"""
+    if num < 10:
+        return singles[num]
+    elif num < 20:
+        return teens[num]
+    elif num < 100:
+        tens_digit = num // 10
+        singles_digit = num % 10
+        if singles_digit == 0:
+            return tens[tens_digit]
+        else:
+            return tens[tens_digit-2] + ' ' + singles[singles_digit]
+
+
+num = int(input("Please enter a number: "))
+
+if num < 100:
+    print(f"Your number is {tens_text(num)}")
+elif num < 1000:
+    print(f"Your number is unknown")
