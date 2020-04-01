44f32635d906b5fc2cfbcc61a54cfa249e289cc0
Devan Fischer <devanf1991@gmail.com>
2020-04-01 13:33:35 -0700

    refactoring and adding user choice

--- Assignments/Devan/1 Python/labs/lab05-random_emoticon_generator.py
+++ Assignments/Devan/1 Python/labs/lab05-random_emoticon_generator.py
@@ -1,23 +1,26 @@
 import random
-
+from huepy import *
 
 
 def _main_():
     how_many = int(input('How many emoticons do you want? '))
-    emoticon = ''
-    for _ in range(how_many):
-        emoticon = make_face()
-        print(emoticon)
+    list_of_emoticons = []
+    seperator = red(' or ')
+    for i in range(how_many):
+        list_of_emoticons.append(make_face())
+    print(seperator.join(list_of_emoticons))
+    user_choice = int(input('Which one do you like best? (1,2,3..)'))
+    print(list_of_emoticons[user_choice - 1])
 
 def make_eyes():
     return random.choice(':;8X')
-    
+
 def make_nose():
     return random.choice('->^•')
-    
+
 def make_mouth():
     return random.choice('D|])(P')
-    
+
 def make_face():
     return make_eyes() + make_nose() + make_mouth()
 
