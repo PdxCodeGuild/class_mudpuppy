70216801e8367f7e122f87ea7392e62182f8a414
Your Name Here <your_email@youremail.com>
2020-03-31 21:08:03 -0700

    adding Lab 14, version 1

--- Assignments/Brea/lab14_v1.py
+++ Assignments/Brea/lab14_v1.py
@@ -2,46 +2,62 @@
 
 import random
 
-my_balance = 0
+winner_card = []
+my_card = []
 
-plays = 10
-
-def pick_six(nums_picked=6):
+def pick_six():
         card = []
-        for _ in range(nums_picked):
+        for _ in range(6):
             card.append(random.choice(range(1, 100)))
         return card
 
-winner_card = pick_six()
-print(winner_card)
-
-my_card = pick_six()
-print(my_card)
-
-def num_matches(card1, card2):
+def num_matches(winner_card, my_card):
     matching_counter = 0
-    for _ in range(6):
-        if card1[_] == card2[_]:
+    for _ in range(len(winner_card)):
+        if winner_card[_] == my_card[_]:
             matching_counter += 1
     return matching_counter
 
-for _ in range(plays):
-    matching_counter = num_matches(winner_card, my_card)
+def winnings(matching_counter, my_balance):
+    if matching_counter == 0:
+        my_balance += 0
+        return my_balance
+    elif matching_counter == 1:
+        my_balance += 4
+        return my_balance
+    elif matching_counter == 2:
+        my_balance += 7
+        return my_balance
+    elif matching_counter == 3:
+        my_balance += 100
+        return my_balance
+    elif matching_counter == 4:
+        my_balance += 50000
+        return my_balance
+    elif matching_counter == 5:
+        my_balance += 1000000
+        return my_balance
+    elif matching_counter == 6:
+        my_balance += 25000000
+        return my_balance
+    else:
+        print("Error, more matches than expected!")
+
+winner_card = pick_six()
+
+plays = 100000
+
+my_balance = 0
+
+for _ in range (plays):
+    my_card = pick_six()
     my_balance -= 2
 
-    if matching_counter == 1:
-        my_balance += 4
-    if matching_counter == 2:
-        my_balance += 7
-    if matching_counter == 3:
-        my_balance += 100
-    if matching_counter == 4:
-        my_balance += 50,000
-    if matching_counter == 5:
-        my_balance += 1,000,000
-    if matching_counter == 6:
-        my_balance += 25,000,000
+    matching_counter = num_matches(winner_card, my_card)
 
-roi_calc = (my_balance - (plays * -2)) /(plays * -2)
+    #my_balance += winnings(matching_counter)
+    my_balance = winnings(matching_counter, my_balance)
+
+roi_calc = round((my_balance - (plays * 2)) /(plays * 2))
 
 print(f"I won ${my_balance}! My ROI was ${roi_calc}")
