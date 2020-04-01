7ad34180524fbd6eae25ae997d738bf6dbcad35e
Devan Fischer <devanf1991@gmail.com>
2020-04-01 13:42:54 -0700

    condensing versions, created class and added actions

--- Assignments/Devan/1 Python/labs/lab08-make_change.py
+++ Assignments/Devan/1 Python/labs/lab08-make_change.py
@@ -1,17 +1,60 @@
-def make_change():
-    user_amount = float(input('How much money do you have (ex. 1.25)? '))
-    user_amount = round(user_amount * 100)
-    if user_amount < 0:
-        print('You must enter a positive number.')
-    else:
-        dollars = user_amount // 100
-        leftover_pennies = user_amount % 100
-        quarters = leftover_pennies // 25
-        dimes = leftover_pennies % 25 // 10
-        nickles = leftover_pennies % 25 % 10 // 5
-        pennies = leftover_pennies % 5
-    print(
-        f'I can give you {dollars} dollars, {quarters} quarters,\n{dimes} dimes, {nickles} nickles, and {pennies} pennies.')
+class Change_Machine:
+    currency = {'dollar': 100, 'quarter': 25, 'dime': 10, 'nickle': 5, 'penny': 1}
+    actions = ['deposit', 'withdraw', 'empty account', 'check balance']
+
+    def actions(self):
+        print('\nWhat would you like to do?\n')
+        user_action = input('Deposit, Withdraw, Empty Account, Check Balance, or Exit. ')
+        user_action = user_action.lower()
+        if user_action == actions[0]:
+            machine.deposit()
+        if user_action == actions[1]:
+            machine.withdraw()
+        if user_action == actions[2]:
+            machine.empty_account()
+        if user_action == actions[3]:
+            machine.receipt()
+
+    def welcome(self):
+        print('Welcome to the change machine.\n')
+        self.balance = float(input('How much money do you have? (ex. 1.75) $'))
+        machine.actions()
+
+    def deposit(self):
+        amount = float(input('\nHow much would you like to add? '))
+        self.balance += amount
+        round(self.balance)
+        print(f'\nYou have ${self.balance} available.')
+        machine.actions()
+
+    def withdraw(self):
+        amount = float(input('\nHow much would you like to withdraw? '))
+        self.balance += (amount * -1)
+        print(f'\nYou have ${self.balance} remaining.')
+        machine.actions()
+
+    def receipt(self):
+        print(f'Your available balance is ${self.balance}')
+        machine.actions()
+
+    def empty_account(self):
+        self.balance = round(self.balance * 100)
+        if self.balance == 0:
+            print('\nSorry, you have no money.')
+        else:
+            dollars = self.balance // currency['dollar']
+            leftover_coins = self.balance % currency['dollar']
+            quarters = leftover_coins // currency['quarter']
+            leftover_coins = leftover_coins % currency['quarter']
+            dimes = leftover_coins // currency['dime']
+            leftover_coins = leftover_coins % currency['dime']
+            nickles = leftover_coins // currency['nickle']
+            pennies = leftover_coins % currency['nickle']
+        print(
+            f'\nI can give you {dollars} dollars, {quarters} quarters, {dimes} dimes, {nickles} nickles, and {pennies} pennies.')
+        print('\nThank you! Bye!')
 
 
-make_change()
+machine = Change_Machine()
+
+machine.welcome()
