672c1575222042e89c2ce53c6b5f32bcacb9747c
Devan Fischer <devanf1991@gmail.com>
2020-04-01 22:50:33 -0700

    added comments

--- Assignments/Devan/1 Python/labs/lab16-image_manipulation.py
+++ Assignments/Devan/1 Python/labs/lab16-image_manipulation.py
@@ -5,15 +5,14 @@ img= Image.open("1 Python/labs/img/mudpuppy.jpeg")
 width, height = img.size
 pixels = img.load()
 
+
 def greyscale():
-    # convert image to greyscale
+    """ Converts an image to greyscale. """
     img_grey = img.convert('L')
     for x in range(width):
         for y in range(height):
             r, g, b = pixels[x, y]
-
             h, s, v = colorsys.rgb_to_hsv(r/255, g/255, b/255)
-
             if h < 0.5 or h > 0.9:
                 s = 0
 
@@ -28,27 +27,23 @@ def greyscale():
     img_grey.save('1 Python/labs/img/mudpuppy_greyscale.jpeg')
     img_grey.show()
 
+
+
 def stick_figure(width, height, head_color='tan'):
-
+    """ Draws a stick figure using width, height, and head color. """
     img = Image.new('RGB', (width, height))
-
     draw = ImageDraw.Draw(img)
 
-
-    # the origin (0, 0) is at the top-left corner
-
-    draw.rectangle(((0, 0), (width, height)), fill="white")
-
-    # draw a rectangle from x0, y0 to x1, y1
+    # fill the background with color
     draw.rectangle(((0, 0), (width, height)), fill="grey")
 
-    # draw a line from x0, y0, x1, y1
+    # draw a body, arms, legs
     color = (256, 128, 128)  # pink
     draw.line(((width/2), (height/4), (width/2), (height-(height/2.5))), fill=color) # body
     draw.line(((width/1.5), (height/2), (width-(width/1.5)), (height/2)), fill=color) # arms
     draw.line(((width/2), (height-(height/2.5)), (width/1.5), (height-(height/6))), fill=color) # l leg
     draw.line(((width/2), (height-(height/2.5)), (width-(width/1.5)), (height-(height/6))), fill=color) # r leg
-
+    # draw a head
     circle_x = width/2
     circle_y = height/4
     circle_radius = width/10
@@ -58,6 +53,5 @@ def stick_figure(width, height, head_color='tan'):
 
 
 stick_figure(600, 600, head_color='red')
-
 
 # greyscale()
