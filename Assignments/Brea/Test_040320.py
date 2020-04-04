65100b1711950c4380d7718bb96cc54727ec87ed
Your Name Here <your_email@youremail.com>
2020-04-03 19:12:16 -0700

    adding Lab 17, version 2, Anagrams

---
+++ Assignments/Brea/Test_040320.py
@@ -0,0 +1,13 @@
+#Test Examples April 3rd, 2020
+
+index = 0
+while index < len(abc_list):    #while loop that adds a 1 to the index values and prints out all values of alphabet
+print(abc_list[index])
+    index += 1
+
+secret_word = [abc_list.index]
+abc_list.index(abc_list.insert())
+
+secret_word = [abc_lst.index('b'), abc_list.index('o'), abc_list.index('o'), abc_list.index('k'))]
+
+print(secret_word)
