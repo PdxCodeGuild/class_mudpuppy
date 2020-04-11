2c3a99f8552a68e01711b7c3820f914c76be57ef
Devan Fischer <devanf1991@gmail.com>
2020-04-10 20:07:45 -0700

    refactored

--- Assignments/Devan/1 Python/labs/lab21_count_words.py
+++ Assignments/Devan/1 Python/labs/lab21_count_words.py
@@ -2,23 +2,38 @@ import os
 import string
 from huepy import *
 
-
 BASE = os.path.dirname(os.path.abspath(__file__))
 
 
-word_counter = {}
-
 with open(os.path.join(BASE, 'TheCalloftheWild.txt')) as book:
-    for line in book.readlines():
-        for punc in string.punctuation:
-            line = line.replace(punc, '').lower().strip()
-        for word in line.split():
-            if word in word_counter.keys():
-                word_counter[word] += 1
-            else:
-                word_counter[word] = 1
+    contents = book.read()
 
-words = list(word_counter.items()) # .items() returns a list of tuples
-words.sort(key=lambda tup: tup[1], reverse=True)  # sort largest to smallest, based on count
-for i in range(min(10, len(words))):  # print the top 10 words, or all of them, whichever is smaller
-    print('The word ' + bold(blue(words[i][0])) + ' was mentioned ' + bold(blue(words[i][1])) + ' times.')
+
+def lower_and_split(contents):
+    results = ""
+    contents = contents.lower()
+    for char in contents:
+        if char not in string.punctuation:
+            results += char
+    results = results.split()
+    return results
+
+
+def count_words(contents):
+    results = lower_and_split(contents)
+    word_counts = {}
+    for word in results:
+        if word not in word_counts:
+            word_counts[word] = 1
+        word_counts[word] += 1
+    return word_counts
+
+
+def top_10(word_counts):
+    words = list(word_counts.items()) # .items() returns a list of tuples
+    words.sort(key=lambda tup: tup[1], reverse=True)  # sort largest to smallest, based on count
+    for i in range(min(10, len(words))):  # print the top 10 words, or all of them, whichever is smaller
+        print('The word ' + bold(blue(words[i][0])) + ' was mentioned ' + bold(blue(words[i][1])) + ' times.')
+
+
+top_10(count_words(contents))
