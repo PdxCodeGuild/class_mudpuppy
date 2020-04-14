d182e3552176337260aad5de62f5e65c3fa5f145
Devan Fischer <devanf1991@gmail.com>
2020-04-13 20:55:14 -0700

    added rating scale

--- Assignments/Devan/1 Python/labs/lab22_ari.py
+++ Assignments/Devan/1 Python/labs/lab22_ari.py
@@ -8,9 +8,26 @@ with open(os.path.join(BASE, 'TheCalloftheWild.txt')) as book:
     text = book.read().lower()
 
 
-def calc_ari(character_count, word_count, sentence_count):
-    text_ari = round(4.71*(character_count/word_count)+0.5*(word_count/sentence_count)-21.43)
-    print(text_ari)
+
+def calc_ari(chars, words, sentences):
+    ari = round(4.71*(chars/words)+0.5*(words/sentences)-21.43)
+    ari_scale = {
+         1: {'ages':   '5-6', 'grade_level': 'Kindergarten'},
+         2: {'ages':   '6-7', 'grade_level':    '1st Grade'},
+         3: {'ages':   '7-8', 'grade_level':    '2nd Grade'},
+         4: {'ages':   '8-9', 'grade_level':    '3rd Grade'},
+         5: {'ages':  '9-10', 'grade_level':    '4th Grade'},
+         6: {'ages': '10-11', 'grade_level':    '5th Grade'},
+         7: {'ages': '11-12', 'grade_level':    '6th Grade'},
+         8: {'ages': '12-13', 'grade_level':    '7th Grade'},
+         9: {'ages': '13-14', 'grade_level':    '8th Grade'},
+        10: {'ages': '14-15', 'grade_level':    '9th Grade'},
+        11: {'ages': '15-16', 'grade_level':   '10th Grade'},
+        12: {'ages': '16-17', 'grade_level':   '11th Grade'},
+        13: {'ages': '17-18', 'grade_level':   '12th Grade'},
+        14: {'ages': '18-22', 'grade_level':      'College'}
+    }
+    print(f"The book is rated for ages {ari_scale[ari]['ages']} or a {ari_scale[ari]['grade_level']} reading level.")
 
 
 def count_characters(text):
