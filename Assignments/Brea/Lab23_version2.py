0e0f300387ea755769150f28a9e01889d2ef2b38
Brea Murakami <brea@ymail.com>
2020-04-14 20:26:14 -0700

    adding Lab 23, version1

---
+++ Assignments/Brea/Lab23_version2.py
@@ -0,0 +1,8 @@
+#Lab 23, version 2, April 14th, 2020
+
+user_input1 = input("What is the contact's name? :")
+user_input2 = input("What is the contact's favorite food? :")
+user_input3 = input("What is the contact's hobby? :")
+user_input4 = input("Whose information would you like to view? :")
+user_input5 = input("Whose information would you like to update? :")
+user_input6 = input(f"Which of ")
