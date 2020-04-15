59f0651ce1e753085956e11524b18006bdd66b23
Devan Fischer <devanf1991@gmail.com>
2020-04-14 20:48:22 -0700

    added print statment for each person

--- Assignments/Devan/1 Python/labs/lab23_contact_list.py
+++ Assignments/Devan/1 Python/labs/lab23_contact_list.py
@@ -8,10 +8,12 @@ contacts = []
 with open(os.path.join(BASE, CSV)) as f:
     lines = f.readlines()
     header = lines[0].strip().split(",")
-    print(header)
+
     for line in lines[1:]:
         line = line.strip().split(",")
         contact = dict(zip(header, line))
         # contact = {header[i]: line[i] for i in range(len(line))}
         contacts.append(contact)
-    print(contacts)
+
+for p in contacts:
+    print(p.get('name') + "'s favorite food is " + p.get('food') + " and they enjoy " + p.get('hobby'))
