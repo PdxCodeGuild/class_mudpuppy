27cf7f4ebf6c48435c16d87e91ff7c117e1ccb41
Eboni Washington <eboni.washington822@gmail.com>
2020-04-14 20:07:21 -0700

    adding dict

--- Assignments/Eboni/lab22.py
+++ Assignments/Eboni/lab22.py
@@ -1,8 +1,9 @@
 import string
+import math
 words = 0
 characters = 0
 lines = 0
-with open("robertfrost2.txt", 'r') as f:
+with open("Gettysburg.txt", 'r') as f:
     for line in f.readlines():
         line = line.lower()
         for punc in string.punctuation:
@@ -11,7 +12,26 @@ with open("robertfrost2.txt", 'r') as f:
         line_list = line.split()
         words = words + len(line_list)
         if lines in line_list:
-          line_list[lines] = lines_list[lines] isn't just new line:
+          line_list[lines] = line_list[lines]
         else: 
             lines +=1
-    print(words,characters,lines)
+    ARI = float(4.71*(characters/words) + 0.5*(words/lines)-21.43)
+    print(ARI)
+# ari_scale = {
+#      1: {'ages':   '5-6', 'grade_level': 'Kindergarten'},
+#      2: {'ages':   '6-7', 'grade_level':    '1st Grade'},
+#      3: {'ages':   '7-8', 'grade_level':    '2nd Grade'},
+#      4: {'ages':   '8-9', 'grade_level':    '3rd Grade'},
+#      5: {'ages':  '9-10', 'grade_level':    '4th Grade'},
+#      6: {'ages': '10-11', 'grade_level':    '5th Grade'},
+#      7: {'ages': '11-12', 'grade_level':    '6th Grade'},
+#      8: {'ages': '12-13', 'grade_level':    '7th Grade'},
+#      9: {'ages': '13-14', 'grade_level':    '8th Grade'},
+#     10: {'ages': '14-15', 'grade_level':    '9th Grade'},
+#     11: {'ages': '15-16', 'grade_level':   '10th Grade'},
+#     12: {'ages': '16-17', 'grade_level':   '11th Grade'},
+#     13: {'ages': '17-18', 'grade_level':   '12th Grade'},
+#     14: {'ages': '18-22', 'grade_level':      'College'}
+# }
+# ages = (ari_scale[ARI])
+# grade_level = (ari_scale[ARI])
