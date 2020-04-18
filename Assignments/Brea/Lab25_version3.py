9b11f54c8879b3e104776bcb62ae2fb1289dd03c
Brea Murakami <brea@ymail.com>
2020-04-17 21:19:19 -0700

    adding Lab 25, version 3

--- Assignments/Brea/Lab25_version3.py
+++ Assignments/Brea/Lab25_version3.py
@@ -1,4 +1,4 @@
-#Lab 25, version 2, April 17th, 2020
+#Lab 25, version 3, April 17th, 2020
 
 class Atm:
     def __init__(account, balance = 0):
@@ -41,7 +41,32 @@ class Atm:
         
 
 Brea = Atm()
-Brea.deposit(100)
-Brea.withdraw(6)
-Brea.deposit(10000)
-Brea.print_transactions()
+
+while True:
+    user_input1 = input("Would you like to deposit, withdraw, check balance, check history, or quit? : ")
+
+    if user_input1 == 'deposit':
+        user_input2 = int(input("How much would you like to deposit? : "))
+        Brea.deposit(user_input2)
+
+    elif user_input1 == 'withdraw':
+        user_input3 = int(input("How much would you like to withdraw? : "))
+        if Brea.balance - user_input3 <= 0:
+            user_input4 = input("This will drain your account. Are you sure? : ")
+            if user_input4 == 'no':
+                break
+            elif user_input4 == 'yes':
+                Brea.withdraw(user_input3)
+
+    elif user_input1 == 'check balance':
+        Brea.check_balance()
+    
+    elif user_input1 == 'check history':
+        Brea.print_transactions()
+    
+    elif user_input1 == 'quit':
+        print("Thank you for banking with the Piggy Bank!")
+        break
+    
+    else:
+        print("Sorry, I don't understand.")
