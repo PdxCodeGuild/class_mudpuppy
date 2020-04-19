88e76e89c718e37ee6cdd1a9c9f0d4636ff1ad8f
Eboni Washington <eboni.washington822@gmail.com>
2020-04-19 11:48:01 -0700

    adding lab 9 updates

--- Assignments/Eboni/lab09-unit_converter.py
+++ Assignments/Eboni/lab09-unit_converter.py
@@ -10,20 +10,20 @@ Ask the user for the number of feet, and print out the equivalent distance in me
 """
 Allow the user to also enter the units. Then depending on the units, convert the distance into meters. The units we'll allow are feet, miles, meters, and kilometers.
 """
-# import math
-# distance = 11
-# user = input("Choose a unit: ")
-# unit_dict = { 'feet' : 0.3048, 'miles': 1609.3, 'meter': 1, 'kilometer': 1000 }
-# user_int = unit_dict[user]
-# conversion = user_int * distance
-# print(f"(The conversion is {conversion} ")
+import math
+distance = int(input("Choose a distance: "))
+user = input("Choose a unit: ")
+unit_dict = { 'feet' : 0.3048, 'miles': 1609.3, 'meter': 1, 'kilometer': 1000 }
+user_int = unit_dict[user]
+conversion = user_int * distance
+print(f"(The conversion is {conversion} ")
 
 """
 Add support for yards, and inches.
 """
 
 # import math
-# distance = 11
+# distance = int(input("Choose a distance: ")
 # user = input("Choose a unit: ")
 # unit_dict = { 'feet' : 0.3048, 'miles': 1609.3, 'meter': 1, 'kilometer': 1000, 'yard' : 0.9144, 'inch' : 0.254 }
 # user_int = unit_dict[user]
