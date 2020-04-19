3ce827426f228a125ed6670bfee63dd63ea40848
Eboni Washington <eboni.washington822@gmail.com>
2020-04-19 11:37:02 -0700

    adding lab12 update

--- Assignments/Eboni/lab12_guessthenumber.py
+++ Assignments/Eboni/lab12_guessthenumber.py
@@ -1,46 +1,48 @@
-# import random
-# computer = random.randint(1,10)
-# def user_guess():
-#     while True:
-#         for i in range (10):
-#             user_guess = int(input(f"What is the computer guess? "))
-#         user_guess = 9-i
-#     if user_guess == computer:
-#         print("Correct! ")
-#     else:
-#         print("Try again! ")
-    
+import random
+computer = random.randint(1,10)
+while True:
+    for i in range (10):
+        i = 9-i
+        user_guess = int(input(f"What is the computer guess? "))
+        if user_guess == computer:
+            print("Correct! ")
+            break
+        elif i == 0:
+            print("You lose! ")
+        else:
+            print("Try again! ")
+
 """
 Version 2
 """
 
-import random 
-computer = random.randint(1,10)
-print(computer)
+# import random 
+# computer = random.randint(1,10)
+# print(computer)
 
-counter1 = 0
-while True:
-    counter1 = int(counter1 + 1)
-    user_guess = (input(f"what is the computer guess? "))
-    if user_guess == 'stop':
-        break
-print(f"You guessed {counter1} times. ")
+# counter1 = 0
+# while True:
+#     counter1 = int(counter1 + 1)
+#     user_guess = (input(f"what is the computer guess? "))
+#     if user_guess == 'stop':
+#         break
+# print(f"You guessed {counter1} times. ")
 
 """
 version 3
 """
-import random 
-computer = random.randint(1,10)
-print(computer)
+# import random 
+# computer = random.randint(1,10)
+# print(computer)
 
-counter1 = 0
-while True:
-    counter1 = int(counter1 + 1)
-    user_guess = (input(f"what is the computer guess? "))
-if user_guess > computer:
-    print("Too high! ")
-elif user_guess < computer:
-    print("Too low! ")
+# counter1 = 0
+# while True:
+#     counter1 = int(counter1 + 1)
+#     user_guess = (input(f"what is the computer guess? "))
+# if user_guess > computer:
+#     print("Too high! ")
+# elif user_guess < computer:
+#     print("Too low! ")
 
 
 
