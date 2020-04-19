0c66a59ee544e8865f2453ba57332744dc671075
Eboni Washington <eboni.washington822@gmail.com>
2020-04-19 11:46:47 -0700

    adding final lab 12

--- Assignments/Eboni/lab12_guessthenumber.py
+++ Assignments/Eboni/lab12_guessthenumber.py
@@ -1,16 +1,16 @@
-import random
-computer = random.randint(1,10)
-while True:
-    for i in range (10):
-        i = 9-i
-        user_guess = int(input(f"What is the computer guess? "))
-        if user_guess == computer:
-            print("Correct! ")
-            break
-        elif i == 0:
-            print("You lose! ")
-        else:
-            print("Try again! ")
+# import random
+# computer = random.randint(1,10)
+# while True:
+#     for i in range (10):
+#         i = 9-i
+#         user_guess = int(input(f"What is the computer guess? "))
+#         if user_guess == computer:
+#             print("Correct! ")
+#             break
+#         elif i == 0:
+#             print("You lose! ")
+#         else:
+            # print("Try again! ")
 
 """
 Version 2
@@ -38,11 +38,11 @@ version 3
 # counter1 = 0
 # while True:
 #     counter1 = int(counter1 + 1)
-#     user_guess = (input(f"what is the computer guess? "))
-# if user_guess > computer:
-#     print("Too high! ")
-# elif user_guess < computer:
-#     print("Too low! ")
+#     user_guess = int(input(f"what is the computer guess? "))
+#     if user_guess > computer:
+#         print("Too high! ")
+#     elif user_guess < computer:
+#         print("Too low! ")
 
 
 
