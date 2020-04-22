ea6d1432c3a1d4153cc45e0078733a9b0ac4ce71
Eboni Washington <eboni.washington822@gmail.com>
2020-04-21 19:11:48 -0700

    adding v2 lab 25

--- Assignments/Eboni/lab25.py
+++ Assignments/Eboni/lab25.py
@@ -2,22 +2,36 @@ class ATM:
     def __init__(self, balance, interest):
         self.balance = balance
         self.interest = interest
+        self.transactions =[]
         # self.check_withdrawl = True
 
     def deposit_amount(self, deposit):
         self.balance += deposit
+        self.transactions.append(f"You deposited {deposit}")
+        # this needs to be copied in a few places
 
-    def withdrawl_amount(self, withdrawl  ):
-        self.balance -= withdrawl
-    
-    def check_withdrawl(self):
-        if self.balance < 0:
-            print("overdraft")
+    def check_withdrawl(self, withdrawl):
+        
+        if self.balance >= withdrawl:
+            print("You can withdraw ")
+            return True
+        else:
+            return False
+
+    def withdrawl_amount(self, withdrawl):
+        if self.check_withdrawl(withdrawl):
+            self.balance -= withdrawl
+        self.transactions.append(f"You withdrew {withdrawl}")
+   
+
+    def calc_interest(self):
+        self.balance += 0.1
+
+    def print_transaction(self):
+        print(self.transactions)
 
 my_atm = ATM(1200, .1)
-my_atm.deposit_amount(1200)
+my_atm.withdrawl_amount(1200)
 ATM.deposit_amount(my_atm, 1200)
 print(my_atm.balance)
-# my_str = 'ABC'
-# print(my_str.lower())
-# print(str.lower(my_str))
+my_atm.print_transaction()
