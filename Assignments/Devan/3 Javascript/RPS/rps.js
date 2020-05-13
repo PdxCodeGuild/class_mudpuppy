8fb24833daba78f244a0c7d08e0fa32aedbb1d2b
Devan Fischer <devanf1991@gmail.com>
2020-05-12 21:29:50 -0700

    v1 of rps in js

---
+++ Assignments/Devan/3 Javascript/RPS/rps.js
@@ -0,0 +1,26 @@
+function randNum(max) {
+    return Math.floor(Math.random() * (max))
+}
+
+function randChoice(inThing) {
+    return inThing[randNum(inThing.length)]
+}
+
+let choices = ["rock", "paper", "scissors"]
+
+let computer = randChoice(choices)
+
+let player = randChoice(choices)
+console.log("computer: " + computer)
+console.log("player: " + player)
+
+if (player == computer)
+    console.log("It's a tie.");
+else if ((player == "rock") && (computer == "scissors"))
+    console.log("You Win!");
+else if ((player == "scissors") && (computer == "paper"))
+    console.log("You Win!");
+else if ((player == "paper") && (computer == "rock"))
+    console.log("You Win!");
+else
+    console.log("You Loose!")
