691f06e475ebf81799fc3fdefea9d9d2445969a5
Eboni Washington <eboni.washington822@gmail.com>
2020-05-13 19:11:52 -0700

    adding part 1 lab 1 of js complete

--- Assignments/Eboni/lab1.js
+++ Assignments/Eboni/lab1.js
@@ -1,22 +1,37 @@
 
 // let userInput = parseInt(prompt("Enter your score: "))
-let outputDiv =document.querySelector("#output")
 let userInput = document.querySelector("#user-input")
+let outputDiv =document.querySelector("#output")
 let scoreBt = document.querySelector("#score_bt")
 
-if ((userInput <=100) && (userInput >=90)){
-    console.log(`You got an A`)
-} 
-else if ((userInput <=89) && (userInput >=80)){
-    console.log(`You got a B`)
+scoreBt.onclick = function() {
+    
+let text
+    
+    if ((userInput.value <=100) && (userInput.value >=90)){
+        text= `You got an A`
+        
+    } 
+    else if ((userInput.value <=89) && (userInput.value >=80)){
+        text= `You got a B`
+    }
+    else if ((userInput.value <=79) && (userInput.value >=70)){
+        text= `You got a C you average fuck`
+    }
+    else if ((userInput.value <=69) && (userInput.value >=60)){
+        text= `You got a D are you even trying?`
+    }
+    else if ((userInput.value <=59) && (userInput.value >=0)){
+        text= `You fail!`
+    }
+    // let output = `You scored ${userInput.value}`
+    // console.log(output)
+    // console.log(userInput)
+    // console.log(userInput.value)
+    outputDiv.innerText = text
 }
-else if ((userInput <=79) && (userInput >=70)){
-    console.log(`You got a C you average fuck`)
-}
-else if ((userInput <=69) && (userInput >=60)){
-    console.log(`You got a D are you even trying?`)
-}
-else if ((userInput <=59) && (userInput >=0)){
-    console.log(`You fail!`)
-}
+  
+
+
+
 
