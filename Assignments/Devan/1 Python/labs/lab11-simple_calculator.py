48e21fc20890619d63bd18d97b58591c092c58ef
Devan Fischer <devanf1991@gmail.com>
2020-05-14 20:34:44 -0700

    commit

--- Assignments/Devan/1 Python/labs/lab11-simple_calculator.py
+++ Assignments/Devan/1 Python/labs/lab11-simple_calculator.py
@@ -1,22 +1,29 @@
 #  Lab 11: Simple Calculator Version 3
 
+
 def get_operator(op):
     return operators[op]
 
-def add(x,y):
+
+def add(x, y):
     return x + y
 
-def subtract(x,y):
+
+def subtract(x, y):
     return x - y
 
-def multiply(x,y):
+
+def multiply(x, y):
     return x * y
 
-def divide(x,y):
+
+def divide(x, y):
     return x / y
+
 
 def eval(fn, n1, n2):
     return fn(n1, n2)
+
 
 operators = {'+': add, '-': subtract, '*': multiply, '/': divide}
 
@@ -29,12 +36,13 @@ while True:
         if operator not in operators:
             print('Not a vaild operator...')
         else:
-            n1 = input('Enter the first number: ') # todo: validate user entered number
+            # todo: validate user entered number
+            n1 = input('Enter the first number: ')
             if n1 is not int:
-            	print('Enter a vaild number. ')
+                print('Enter a vaild number. ')
             n2 = int(input('Enter the second number: '))
             if n2 is not int:
-            	print('Enter a valid number. ')
+                print('Enter a valid number. ')
             print(eval(get_operator(operator), int(n1), int(n2)))
     except KeyboardInterrupt:
         print('\nThank You')
