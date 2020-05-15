ba0f495c3c50a22e7e4d8faf3cc64de51ca9f00c
Devan Fischer <devanf1991@gmail.com>
2020-05-14 20:35:18 -0700

    fixed some admin issues

--- Assignments/Devan/4 Django/personal_portfolio/blog/models.py
+++ Assignments/Devan/4 Django/personal_portfolio/blog/models.py
@@ -6,6 +6,9 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=20)
 
+    def __str__(self):
+        return self.name
+
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
@@ -13,6 +16,9 @@ class Post(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
     categories = models.ManyToManyField('Category', related_name='posts')
+
+    def __str__(self):
+        return self.title
 
 
 class Comment(models.Model):
