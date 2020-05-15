f08c55fef6b90ca0f46ff1e6579da1b05d69c65e
Devan Fischer <devanf1991@gmail.com>
2020-05-14 20:34:29 -0700

    addition code along

---
+++ Assignments/Devan/3 Javascript/* Code Along/addition_tester_mudpuppy/additiontester.js
@@ -0,0 +1,41 @@
+function randInt(lowerNum, upperNum) {
+    return lowerNum + Math.floor(
+        Math.random() * ((upperNum + 1) - lowerNum)
+    )
+}
+
+let mathProblem = document.querySelector(".math-problem")
+let output = document.querySelector(".output")
+let userAnswer = document.querySelector(".user-answer")
+let enterButton = document.querySelector(".enter-button")
+
+let num1 = randInt(1, 50)
+let num2 = randInt(1, 50)
+mathProblem.innerText = `What is ${num1} + ${num2}? `
+
+
+enterButton.addEventListener("click", function () {
+    if (parseInt(userAnswer.value) === num1 + num2) {
+        output.innerText = "Correct!"
+    } else {
+        output.innerText = "What? Try again, you fool!!"
+    }
+    setTimeout(() => {
+        location.reload()
+    }, 1000)
+});
+
+userAnswer.addEventListener("keyup", function (event) {
+    if (event.keyCode == 13) {
+        enterButton.click();
+    }
+})
+
+// enterButton.onclick = function () {
+//     if (parseInt(userAnswer.value) === num1 + num2) {
+//         output.innerText = "Correct!"
+//     } else {
+//         output.innerText = "What? Refresh, you fool!!"
+//     }
+//     location.reload();
+// }
