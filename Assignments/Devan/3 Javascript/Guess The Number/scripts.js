6caad619e4ab2d72d1b883d1fea5a3b667685d42
Devan Fischer <devanf1991@gmail.com>
2020-05-14 21:22:19 -0700

    started building script

--- Assignments/Devan/3 Javascript/Guess The Number/scripts.js
+++ Assignments/Devan/3 Javascript/Guess The Number/scripts.js
@@ -1,1 +1,17 @@
-{}
+function randInt(lowerNum, upperNum) {
+    return lowerNum + Math.floor(
+        Math.random() * ((upperNum + 1) - lowerNum)
+    )
+}
+
+let guessBetween = document.querySelector(".guess-between")
+let userGuess = document.querySelector(".user-guess")
+let submitButton = document.querySelector(".submit-button")
+let hintDiv = document.querySelector(".hint-div")
+
+let maxNum = 100
+let minNum = 1
+var guessCount = 0
+let lastGuess = null
+let correctNum = randInt(minNum, maxNum)
+guessBetween.innerText = `Guess a number between ${minNum} and ${maxNum}: `
