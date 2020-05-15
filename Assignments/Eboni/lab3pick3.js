cb5a371bde621de75807b09abb2e3626ee01a8f2
Eboni Washington <eboni.washington822@gmail.com>
2020-05-14 20:48:16 -0700

    adding lab3 pick 3

---
+++ Assignments/Eboni/lab3pick3.js
@@ -0,0 +1,33 @@
+
+let choices=['r', 'p', 's']
+let computerGuess = choices[Math.floor(Math.random() *3)]
+let userInput =document.querySelector("#user-input")
+let outputDiv =document.querySelector("#output")
+let guessBt =document.querySelector("#guess-bt")
+
+
+guessBt.onclick =function() {
+
+let text
+    if (userInput.value == computerGuess) {
+        text=`You tie`
+    }
+    else if (userInput.value == 'r' && computerGuess == 'p') {
+        text= `You lose`
+    }
+    else if (userInput.value == 'p' && computerGuess == 's') {
+        text=`You lose`
+    }    
+    else if (userInput.value == 's' && computerGuess == 'p') {
+        text= `You win`
+    }
+    else if (userInput.value == 'r' && computerGuess == 's') {
+        text='You win'
+    }
+    
+    
+
+    
+outputDiv.innerText = text + `\nThe computer guessed: ${computerGuess}`
+}
+
