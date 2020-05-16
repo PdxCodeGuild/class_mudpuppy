603c8cb87b8585e0e0459f6387ccd316946dabf7
Devan Fischer <devanf1991@gmail.com>
2020-05-15 19:31:59 -0700

    changed max num

--- Assignments/Devan/3 Javascript/Guess The Number/scripts.js
+++ Assignments/Devan/3 Javascript/Guess The Number/scripts.js
@@ -3,7 +3,7 @@ let userGuess = document.querySelector(".user-guess")
 let submitButton = document.querySelector(".submit-button")
 let hintDiv = document.querySelector(".hint-div")
 
-let maxNum = 100
+let maxNum = 10
 let minNum = 1
 var guessCount = 0
 let lastGuess = null
