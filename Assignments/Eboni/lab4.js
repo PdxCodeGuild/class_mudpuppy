bc0a8d53ef6e43e122fa20402b9e44b97450ef16
Eboni Washington <eboni.washington822@gmail.com>
2020-05-20 18:06:29 -0700

    adding lab 4 js

--- Assignments/Eboni/lab4.js
+++ Assignments/Eboni/lab4.js
@@ -1,8 +1,33 @@
 let input= document.querySelector("#input")
 let button = document.querySelector("#add-to-list")
 let myList = document.querySelector("#my-list")
+// let removeButton = document.querySelector("#remove-button")
+// let completedList = document.querySelector("#completed-list")
 button.onclick =function() {
-    let newDiv = document.createElement("li")
-        newDiv.innerText= input.value
-       myList.appendChild(newDiv) 
+    let newli = document.createElement("li")
+    let newSpan = document.createElement("span")
+    let newButton = document.createElement("button")
+    newli.appendChild(newSpan)
+    newli.appendChild(newButton)
+    myList.appendChild(newli)
+         newSpan.innerText= input.value
+         newButton.innerText = "Remove"
+    //     myList.appendChild(newDiv)
+            newButton.onclick =function() {
+                this.parentNode.parentNode.removeChild(this.parentNode)
+                
+            }
+
+    // let newDiv = document.createElement
+    // ("div")
+    
+    // newButton.innerText = "Remove"
+    // newSpan.innerText = input.value
+
+    
+    // newButton.onclick = function() {
+    //     numSpan = this.parentNode.firstChild
+    //     numSpan.innerText = parseInt(numSpan.innerText)
+    // }
 }
+
