02f3adee053ee96bf73fff1b265c83ba4bedf8d7
Devan Fischer <devanf1991@gmail.com>
2020-05-21 20:32:26 -0700

    added countdown

--- Assignments/Devan/3 Javascript/Random Website/scripts.js
+++ Assignments/Devan/3 Javascript/Random Website/scripts.js
@@ -18,5 +18,27 @@ function randomSite(sitesArr) {
     return sitesArr[Math.floor(Math.random() * sitesArr.length)]
 }
 
+function startTimer(duration, display) {
+    var timer = duration,
+        minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
 
-window.location.replace(randomSite(sitesArr))
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = seconds;
+
+        if (--timer < 0) {
+            window.location.replace(randomSite(sitesArr))
+            // timer = duration;
+        }
+    }, 1000);
+}
+
+window.onload = function () {
+    var fiveSeconds = 5
+    display = document.querySelector('#countdown-timer');
+    startTimer(fiveSeconds, display);
+};
