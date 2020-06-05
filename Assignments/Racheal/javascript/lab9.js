617609dc4afd1b6bd8f1f8f35834c2312cd1eee1
rachealnakhabala <nakhabala.racheal@gmail.com>
2020-06-04 21:19:26 -0700

    first commit

---
+++ Assignments/Racheal/javascript/lab9.js
@@ -0,0 +1,21 @@
+<html> 
+    <head>
+        <meta charset="utf-8">
+        <script scr="http://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"><</script>
+    <head>
+    <body>
+        <h1> id="author"></h1>
+        <ul id="quote.output"><</ul>
+    <script> 
+        let quoteOutput= document.querySelector("#quote-output");
+        let authorStr = "james+baldwin";
+        /*
+        axios({
+            method: 'get' ,
+            url: https://favqs.com/api/quotes/?filter=${authorStr}&type=author`,
+            headers: {
+                Authorization:  Token token=ca29750c8b4c93e8a2b297b3f7e7616",
+        })
+    </script>
+    </body>
+</html>
