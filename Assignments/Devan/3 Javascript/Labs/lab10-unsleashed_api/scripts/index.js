69b3a8c4bf93296f1613be7afd4300b2c0bdff5b
Devan Fischer <devan@createpnw.com>
2020-06-04 22:58:27 -0700

    added more params and set them as variables

--- Assignments/Devan/3 Javascript/Labs/lab10-unsleashed_api/scripts/index.js
+++ Assignments/Devan/3 Javascript/Labs/lab10-unsleashed_api/scripts/index.js
@@ -8,9 +8,14 @@ require('dotenv').config()
 let API_ID = process.env.API_ID
 let API_KEY = process.env.API_KEY
 
+/* NOTE: Definging search params, they still need to be
+        added to the axios method and the urlParam */
+
+let ORDER_STATUS = 'Parked'
+let PAGE_SIZE = 2
 
 function getSalesOrders() {
-    let urlParam = "orderStatus=Backordered";
+    let urlParam = `orderStatus=${ORDER_STATUS}&pageSize=${PAGE_SIZE}`;
     var hash = CryptoJS.HmacSHA256(urlParam, API_KEY);
     var hash64 = CryptoJS.enc.Base64.stringify(hash);
 
@@ -18,7 +23,8 @@ function getSalesOrders() {
         method: 'GET',
         url: 'https://api.unleashedsoftware.com/SalesOrders',
         params: {
-            orderStatus: 'Backordered'
+            orderStatus: ORDER_STATUS,
+            pageSize: PAGE_SIZE,
         },
         headers: {
             'Accept': 'application/json',
@@ -28,7 +34,14 @@ function getSalesOrders() {
         },
     }).then(function (response) {
         console.log(Object.keys(response))
-        console.log(response.data)
+        var salesOrders = response.data.Items // NOTE: An array of the orders
+        var orderLines = salesOrders[0].SalesOrderLines // NOTE: The firt index of salesOrders, a array of the sales lines
+        console.log(salesOrders)
+        orderLines.forEach(line => {
+            if (line.Product.ProductCode === 'HB-VF-01') {
+                console.log(line.Product.ProductCode)
+            }
+        });
     }).catch(function (error) {
         console.log(error)
     })
