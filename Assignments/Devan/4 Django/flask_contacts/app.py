f230ae7c0f1b455df8ebf91f3da89a0e9fb0bddd
Devan Fischer <devan@createpnw.com>
2020-06-08 19:26:31 -0700

    finished contacts lab

--- Assignments/Devan/4 Django/flask_contacts/app.py
+++ Assignments/Devan/4 Django/flask_contacts/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 import random
+from contacts import Contact
 app = Flask(__name__)
 
 
@@ -24,4 +25,39 @@ def hello_world():
     output = output.replace('name_var', contact[0])
     output = output.replace('food_var', contact[1])
     output = output.replace('game_var', contact[2])
+    return output
+
+
+@app.route('/contacts/<name>/')
+def by_name(name):
+    print(name)
+    output = '''
+    <h1>Info about name_var</h1>
+    <ul>
+        <li>Food: food_var</li>
+        <li>Game: game_var</li>
+    </ul>
+    '''
+    found_contact = Contact.find_contact(name)
+    output = output.replace('name_var', found_contact.name_attr)
+    output = output.replace('food_var', found_contact.food_attr)
+    output = output.replace('game_var', found_contact.game_attr)
+    return output
+
+
+@app.route('/contacts/id/<contact_id>/')
+def by_id(contact_id):
+    print(contact_id)
+
+    output = '''
+    <ul>
+        <li>Food: food_var</li>
+        <li>Game: game_var</li>
+    </ul>
+    '''
+
+    found_contact = Contact.find_by_id(contact_id)
+    output = output.replace('name_var', found_contact.name_attr)
+    output = output.replace('food_var', found_contact.food_attr)
+    output = output.replace('game_var', found_contact.game_attr)
     return output
