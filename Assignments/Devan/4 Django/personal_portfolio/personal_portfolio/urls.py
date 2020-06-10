966bc6bdaa0f5c3ff19f2a3ce63584889f8ded87
Devan Fischer <devan@createpnw.com>
2020-06-09 19:29:33 -0700

    added polls app to proj

--- Assignments/Devan/4 Django/personal_portfolio/personal_portfolio/urls.py
+++ Assignments/Devan/4 Django/personal_portfolio/personal_portfolio/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("projects/", include("projects.urls")),
     path("blog/", include("blog.urls")),
+    path("polls/", include("polls.urls")),
 ]
