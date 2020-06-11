857ef1b0d7c5fbbfcc809aedb102df372f469550
Devan Fischer <devan@createpnw.com>
2020-06-10 20:34:28 -0700

    added forms and generic views

--- Assignments/Devan/4 Django/personal_portfolio/polls/admin.py
+++ Assignments/Devan/4 Django/personal_portfolio/polls/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Question, Choice
 
 
 class PollsAdmin(admin.ModelAdmin):
@@ -8,3 +8,4 @@ class PollsAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Question)
+admin.site.register(Choice)
