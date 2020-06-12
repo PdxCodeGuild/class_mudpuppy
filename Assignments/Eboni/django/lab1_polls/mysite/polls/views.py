dbfe385d74f4f5b26dccbd1052aa902ff4f971a7
Eboni Washington <eboni.washington822@gmail.com>
2020-06-11 19:53:13 -0700

    adding pt 4 lab 1

--- Assignments/Eboni/django/lab1_polls/mysite/polls/views.py
+++ Assignments/Eboni/django/lab1_polls/mysite/polls/views.py
@@ -1,10 +1,21 @@
-# from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
-# from django.shortcuts import render
-# from django.http import HttpResponse
-# from django.template import loader
+from django.urls import reverse
 
 from .models import Question
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -18,8 +29,8 @@ def detail(request, question_id):
     
 
 def results(request, question_id):
-    response = "Youre looking at the reults of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
     return HttpResponse("Youre' voting on %s.")
