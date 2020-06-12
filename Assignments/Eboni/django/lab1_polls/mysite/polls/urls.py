28a714a311eef625a62e40318b62875bd8c9a323
Eboni Washington <eboni.washington822@gmail.com>
2020-06-11 21:33:24 -0700

    adding final lab1

--- Assignments/Eboni/django/lab1_polls/mysite/polls/urls.py
+++ Assignments/Eboni/django/lab1_polls/mysite/polls/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
 
     path('<int:question_id>/', views.detail, name='detail'),
 
-    path('<int:question_id>/reulsts/', views.results, name='results'),
+    path('<int:question_id>/results/', views.results, name='results'),
 
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
