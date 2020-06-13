11108ead1bdc42ae70dbbafcff4983c78f4cbfcd
Devan Fischer <devan@createpnw.com>
2020-06-12 19:22:54 -0700

    finishd pt 6

--- Assignments/Devan/4 Django/links_django/links_app/views.py
+++ Assignments/Devan/4 Django/links_django/links_app/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Link
 
 
 # Create your views here.
 
+
 def index(request):
-    return HttpResponse('hello')
+    all_links = Link.objects.all()
+    context = {
+        'all_links_template': all_links,
+    }
+    return render(request, 'links_app/index.html', context)
+
+
+def link_slug(request, in_slug):
+    found_link = Link.objects.get(slug=in_slug)
+    comments = ['its great', 'its bad']
+    context = {
+        'found_link_template': found_link,
+        'comments_template': comments,
+    }
+    return render(request, 'links_app/slug.html', context)
