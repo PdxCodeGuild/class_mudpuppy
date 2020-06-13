609999456a4fe9207a44190a6007bed33f4fddae
Brea Murakami <brea@ymail.com>
2020-06-12 21:13:00 -0700

    adding the links guide and starting progress for Django Lab 2

---
+++ Assignments/Brea/Django Labs/todo_django/todo_app/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here. This is where I start building the database, it's the first thing to do.
+class ToDoItem(models.Model):
+    todo_text = 
+    create_date =
+    complete_date =
+    completed_bool =
