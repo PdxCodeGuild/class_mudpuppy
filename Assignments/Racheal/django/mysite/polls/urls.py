e3d8a99b48d7eabd48b44f52329ce39beabe775e
rachealnakhabala <nakhabala.racheal@gmail.com>
2020-06-16 21:16:34 -0700

    lab1 complete

--- Assignments/Racheal/django/mysite/polls/urls.py
+++ Assignments/Racheal/django/mysite/polls/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 
 from . import views 
 
+app_name = 'polls'
 urlpatterns = [
     #ex: /polls/
-    # path( '', views.index, name ='index'),
+    path( '', views.index, name ='index'),
     #ex: /polls/5/
     path('<int:question_id>/', views.detail, name='detail'),
     #ex: /polls/5/results/
--- Assignments/Racheal/django/mysite/polls/views.py
+++ Assignments/Racheal/django/mysite/polls/views.py
@@ -1,16 +1,21 @@
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+
+from .models import Question 
+# from django.http import HttpResponse
+# from django.template import loader
 
 from .models import Question
 
 
 # detail(request=<HttpRequest object>, question_id=34)
 def index(request):
-    latest_question_list = Question.object.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': 
+    latest_question_list}
+    return render(request, 'polls/index.html', context)
 def detail(request, question_id): 
-    print(f"you typed {question_id}")
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Qeustion, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 def results (request, question_id):
     response = "You're looking at the results of question %s."
     return HttpResponse(response % question_id)
