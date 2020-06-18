c21ce836bf9308ee1b487b9acaa4ba40117e84a0
Eboni Washington <eboni.washington822@gmail.com>
2020-06-17 21:22:28 -0700

    adding to do list

--- Assignments/Eboni/django/todo_list/todo_list/todo_app/views.py
+++ Assignments/Eboni/django/todo_list/todo_list/todo_app/views.py
@@ -1,6 +1,19 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, reverse
+from django.http import HttpResponseRedirect
+from .models import TodoItem
 
 def index(request):
-    return HttpResponse('Hello World')
+    display = list(TodoItem.objects.all())
+    add_item = display
+    context = {'list_template_var': add_item}
+    return render(request, 'todo_app/index.html', context)
+
+def add_list(request):
+    print(request.body)
+    print(request.POST["add_item"])
+    data = request.POST
+    new_todo = TodoItem(item_text=data["add_item"])
+    new_todo.save()
+    return HttpResponseRedirect(reverse("todo:index"))
+
 # Create your views here.
