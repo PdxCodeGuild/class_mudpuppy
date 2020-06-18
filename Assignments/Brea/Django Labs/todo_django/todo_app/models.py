90625bc7e3cab60a2e204747e3ac0288a9dcf5ce
Brea Murakami <brea@ymail.com>
2020-06-17 21:30:23 -0700

    adding Django Lab 2

--- Assignments/Brea/Django Labs/todo_django/todo_app/models.py
+++ Assignments/Brea/Django Labs/todo_django/todo_app/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here. This is where I start building the database, it's the first thing to do.
 class ToDoItems(models.Model):
     todo_text = models.CharField(max_length=150)
-    create_date = models.DateField('date started', auto_now=True)
-    complete_date = models.DateField('date completed', blank=True, null=True)
+    create_date = models.DateField('date started', auto_now_add=True)
+    complete_date = models.DateField('date completed', blank=True, null=True, auto_now=False)
     completed_bool = models.BooleanField(default=False)
 
     def __str__(self):
