798f066d353c3b8af2da1519bc2dca0bb2d1458f
Manuel Trujillo Maciel <manueltrujillomaciel@Manuels-MacBook-Pro.local>
2020-06-22 21:28:57 -0700

    end of day push

--- Assignments/Manny/Django/links_django/links_app/links_urls.py
+++ Assignments/Manny/Django/links_django/links_app/links_urls.py
@@ -7,16 +7,3 @@ urlpatterns = [
     path('l/<str:in_slug>/', views.link_slug, name="slug_path"), # New line
     path('comment/', views.add_comment, name="comment_path",),
 ]
- {% for comment in comments_template %}
-               <div class="comment-div">
-                       {{ comment.text }} # Changed line
-               </div>
-               {% endfor %}
-               <form method="POST" action="{% url 'links_paths:comment_path' %}"> # New line
-                       {% csrf_token %} # New line
-                       <input type="hidden" name="link" value="{{found_link_template.id}}"></input> # New line
-                       <textarea placeholder="Type your comment here" name="commenttext"></textarea> # New line
-                       <button>submit</button> # New line
-               <form> # New line
-        </body>
- </html>
