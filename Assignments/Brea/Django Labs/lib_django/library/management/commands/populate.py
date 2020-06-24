c58722be3c0c47f894766c1964f368c681503b0b
Brea Murakami <brea@ymail.com>
2020-06-23 21:11:13 -0700

    adding Django Lab 3 and start to Lib Guide

---
+++ Assignments/Brea/Django Labs/lib_django/library/management/commands/populate.py
@@ -0,0 +1,23 @@
+from django.core.management.base import BaseCommand, CommandError
+from library.models import Book, Author
+from datetime import datetime
+
+class Command(BaseCommand):
+    help = 'Populates the Database'
+
+    def handle(self, *args, **options):
+        author1 = Author.objects.get_or_create(last_name="Tolkien", first_name="J.", middle1="R.", middle2="R.")[0]
+        author2 = Author.objects.get_or_create(last_name="Machado", first_name="Carmen", middle1="Maria")[0]
+
+        books = (
+            ('The Lord of the RIngs', datetime.strptime('29-07-1954', '%d-%m-%Y'), author1),
+            ('The Hobbit', datetime.strptime('21-09-1937', '%d-%m-%Y'), author1),
+            ('In the Dream House', datetime.strptime('05-11-2019', '%d-%m-%Y'), author2),
+        )
+
+        for one_book in books:
+            Book.objects.get_or_create(
+                title = one_book[0],
+                publish_date = one_book[1],
+                author = one_book[2],
+            )
---
+++ Assignments/Brea/Django Labs/lib_django/library/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=128)
+    publish_Date = models.DateField()
+    author = models.ForeignKey('Author', on_delete=models.PROTECT)
+
+class Author(models.Model):
+    last_name = models.CharField(max_length=32)
+    first_name = models.CharField(max_length=32)
+    middle1 = models.CharField(max_length=16, blank=True, null=True)
+    middle2 = models.CharField(max_length=16, blank=True, null=True)
+
+    def name_formatted(self):
+        output = f"{self.last_name}, {self.first_name}"
+        if self.middle1:
+            output += f" {self.middle1}"
+            if self.middle2:
+                output += f" {self.middle2}"
+        return output
