7296f5f3e289b52aee30c397414f255a846b3d10
Manuel Trujillo Maciel <manueltrujillomaciel@Manuels-MacBook-Pro.local>
2020-06-24 20:13:10 -0700

    Mid day push

--- Assignments/Manny/Django/lab02_todo/todo_app/models.py
+++ Assignments/Manny/Django/lab02_todo/todo_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class ToDo(models.Model):
+    task = models.CharField(max_length=32)
+    created = models.DateTimeField(auto_now_add=True) #auto now add for created
+    mod = models.DateTimeField(auto_now=True)
+    complete = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.task
+
+# class DateTimeField(auto_now=False, auto_now_add=False, **options) 
+# class BooleanField(**options)
+# class CharField(max_length=None, **options)
--- Assignments/Manny/Django/lab02_todo/todo_app/views.py
+++ Assignments/Manny/Django/lab02_todo/todo_app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse 
+from . import models
 
-# Create your views here.
+def index(request):
+    print(models.ToDo.objects.all())
+    return HttpResponse('test')
