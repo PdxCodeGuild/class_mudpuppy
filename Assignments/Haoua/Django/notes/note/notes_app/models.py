e6c2135daf9af885e037538d6f82fb74f8fe12da
Haoua <haouangoyed@gmail.com>
2020-06-26 20:56:35 -0700

    626

---
+++ Assignments/Haoua/Django/notes/note/notes_app/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Note(models.Model):
+    text = models.CharField(max_length=128)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='note')
+
+# Create your models here.
