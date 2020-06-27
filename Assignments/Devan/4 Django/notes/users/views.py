493085bfccf6f9e0f7a6d6cc85e7b708d9a90c0e
Devan Fischer <devanf1991@gmail.com>
2020-06-26 20:56:39 -0700

    fixed new user method

--- Assignments/Devan/4 Django/notes/users/views.py
+++ Assignments/Devan/4 Django/notes/users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, reverse
 from django.http import HttpResponseRedirect
 from django.contrib.auth import login as _login, logout as _logout, authenticate as auth
+from django.contrib.auth.models import User
 
 def login(request):
     username = request.POST['username']
@@ -15,7 +16,12 @@ def logout(request):
     return HttpResponseRedirect(reverse('users:rl_page'))
 
 def register(request):
-    pass
+    username = request.POST['username']
+    email = request.POST['email']
+    password = request.POST['password']
+    new_user = User.objects.create_user(username, email, password)
+    _login(request, new_user)
+    return HttpResponseRedirect(reverse('notes_app:index'))
 
 def rl_page(request):
     next_place = request.GET.get('next', reverse('notes_app:index'))
