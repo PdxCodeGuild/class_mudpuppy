046c03df01e1f1bb105608e122fbc7d2769c674b
Devan Fischer <devanf1991@gmail.com>
2020-06-30 19:43:25 -0700

    changed models

--- Assignments/Devan/4 Django/lab03-url_shortener/url_app/models.py
+++ Assignments/Devan/4 Django/lab03-url_shortener/url_app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class URL(models.Model):
     full_url = models.URLField(unique=True)
-    short_url = models.CharField(max_length=6)
+    tiny_url = models.CharField(max_length=6)
     created_at = models.DateTimeField(auto_now_add=True)
     clicks = models.IntegerField(default=0)
 
