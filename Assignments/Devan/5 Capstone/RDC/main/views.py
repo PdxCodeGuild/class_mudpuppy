6e5b20b284b86ab8a981b8206b6b9147dde11593
Devan Fischer <devanf1991@gmail.com>
2020-07-01 21:18:25 -0700

    starting to add materialize

--- Assignments/Devan/5 Capstone/RDC/main/views.py
+++ Assignments/Devan/5 Capstone/RDC/main/views.py
@@ -4,4 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello")
+    context= {
+
+    }
+    return render(request, 'main/index.html', context)
