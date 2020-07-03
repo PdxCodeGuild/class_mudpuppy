008222ac7b045ae83745feac09244883743a47a3
Brea Murakami <brea@ymail.com>
2020-07-02 21:03:18 -0700

    adding updated Models and Input form

--- Assignments/Brea/Capstone/capstone/models.py
+++ Assignments/Brea/Capstone/capstone/models.py
@@ -1,41 +1,70 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Artist(models.Model):
+    name = models.CharField(max_length=128)
+
 class Song(models.Model):
     song_name = models.CharField(max_length=128)
-    artist_name = models.CharField(max_length=128)
-    release_year = models.IntegerField(null=True)
-    spotify_link = models.URLField(null=True)
-    chords_link = models.URLField(null=True)
-    info_link = models.URLField(null=True)
+    artist = models.ManyToManyField(Artist)
+    release_year = models.IntegerField(null=True, blank=True)
+    spotify_link = models.URLField(null=True, blank=True)
+    chords_link = models.URLField(null=True, blank=True)
+    info_link = models.URLField(null=True, blank=True)
 
-    # song_tags = models.ManyToManyField()
+class Tags(models.Model):
+    class Modes(models.TextChoices):
+        Major = 'MJ', _('Major')
+        Minor = 'mn', _('Minor')
+        Other = 'Ot', _('Other')
 
-# class Mus_Tags(models.Model):
-#     mode_choices = [
-#         (Major, 'Major'),
-#         (Minor, 'Minor'),
-#         (Other, 'Other')
-#     ]
-#     genre = models.Charfield(max_length=50)
-#     tempo = models.IntegerField()
-#     singability = models.IntegerField()
-#     harmonic_comp = models.IntegerField()
-#     repetition = model.IntegerField()
-#     mode = models.CharField(max_length=5, choices=mode_choices)
+    class Genres(models.TextChoices):
+        Blues = 'bls', _('Blues')
+        Classical = 'cls', _('Classical/Art')
+        Country = 'ctr', _('Country')
+        Easy = 'eas', _('Easy Listening')
+        Electronic = 'elc', _('Electronic')
+        Folk = 'flk', _('Folk')
+        Gospel = 'gos', _('Gospel/Soul')
+        Hiphop = 'hip', _('Hip-hop/Rap')
+        Instrumental = 'ins', _('Instrumental')
+        Jazz = 'jzz', _('Jazz')
+        Latin = 'ltn', _('Latin')
+        Metal = 'mtl', _('Metal')
+        Musical = 'brd', _('Musical/Broadway')
+        Pop = 'pop', _('General Pop')
+        RB = 'r&b', _('Rhythm and Blues')
+        Reggae = 'reg', _('Reggae')
+        Rock = 'rck', _('Rock')
+        World = 'wrl', _('World Music')
+        Other = 'oth', _('Other')
+        
+    genre = models.CharField(
+        max_length=20,
+        choices=Genres.choices,
+        null=True, blank=True,)
+    
+    mode = models.CharField(
+        max_length=5,
+        choices=Modes.choices,
+        null=True, blank=True,)
 
-# class NonMus_Tags(models.Model):
-#     mention_choices = [
-#         (Drugs, 'Drugs'),
-#         (Sex, 'Sex'),
-#         (Self-Harm, 'Self-Harm'),
-#         (Other, 'Other')
-#     ]
-#     arousal = models.IntegerField()
-#     # emotions = pass
-#     # age_category = pass
-#     mentions = models.CharField(max_length=10, choices=mention_choices)
-#     cultural_info = models.TextField()
-#     interventions_ideas = models.TextField()
+    tempo = models.IntegerField(null=True, blank=True)
+    singability = models.IntegerField(null=True, blank=True)
+    playability = models.IntegerField(null=True, blank=True)
+    repetition = models.IntegerField(null=True, blank=True)
+    dynamics = models.IntegerField(null=True, blank=True)
 
+    arousal = models.IntegerField(null=True, blank=True)
+    popularity = models.IntegerField(null=True, blank=True)
+    valence = models.IntegerField(null=True, blank=True)
+    emotion = models.CharField(max_length=50,null=True, blank=True)
+    theme = models.CharField(max_length=50, null=True, blank=True)
+    comments = models.TextField(null=True, blank=True)
+
+class SongReview(models.Model):
+    song = models.ForeignKey(Song, on_delete=models.PROTECT)
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
 
