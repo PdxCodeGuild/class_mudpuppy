24572c55475c635df91f0083371541226e53121d
emteebit <manueltrujillomaciel@Manuels-MacBook-Pro.local>
2020-07-04 15:27:59 -0700

    notes example and some databse

--- Assignments/Manny/Django/notes/notes/models.py
+++ Assignments/Manny/Django/notes/notes/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 class Note(models.Model):
     text = models.CharField(max_length=128)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notes')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notes_tacos')
 
 
 # Create your models here.
