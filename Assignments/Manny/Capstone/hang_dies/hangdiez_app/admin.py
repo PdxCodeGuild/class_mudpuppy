a1f910c5284531bd00448c6ab7a06584ee0fb470
emteebit <manueltrujillomaciel@Manuels-MacBook-Pro.local>
2020-07-06 21:17:07 -0700

    end push

--- Assignments/Manny/Capstone/hang_dies/hangdiez_app/admin.py
+++ Assignments/Manny/Capstone/hang_dies/hangdiez_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import State, UserProfile
+admin.site.register(State) 
+admin.site.register(UserProfile)
 
-# Register your models here.
+
+
--- Assignments/Manny/Capstone/hang_dies/hangdiez_app/diezapp_urls.py
+++ Assignments/Manny/Capstone/hang_dies/hangdiez_app/diezapp_urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = 'users'
 urlpatterns = [
-       path('', views.index)
+    path('rl-page/', views.rl_page, name='rl_page' ),
+    path('register/',views.register, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
 ]
