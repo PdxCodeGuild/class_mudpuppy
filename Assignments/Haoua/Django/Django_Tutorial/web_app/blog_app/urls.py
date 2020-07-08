c13c83dacc094863abb14f985b6b70e31c6c80de
haoua <haouangoyed@gmail.com>
2020-07-07 20:14:22 -0700

    testing

--- Assignments/Haoua/Django/Django_Tutorial/web_app/blog_app/urls.py
+++ Assignments/Haoua/Django/Django_Tutorial/web_app/blog_app/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='blog_home'),
-    path('about/', views.about, name='blog_abpit'),
+    path('about/', views.about, name='blog_about'),
 
     
 ]
