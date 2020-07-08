c716c566a8fd9b10ce0857e7088581603537793c
Eboni Washington <eboni.washington822@gmail.com>
2020-07-07 21:02:38 -0700

    adding api test, and if loops for database

--- Assignments/Eboni/Capstone/horoscope_app/views.py
+++ Assignments/Eboni/Capstone/horoscope_app/views.py
@@ -9,9 +9,62 @@ def index(request):
     return render(request, "horoscope_app/index.html") 
 
 def detail(request):
-    if request.method == "POST":
-        print(api_key, api_id)
-    return render(request, "horoscope_app/detail.html")
+    print(request.POST)
+    date = request.POST["new"]
+    print(date.split('-'))
+    date_list = date.split('-')
+    month = int(date_list[1])
+    day = int(date_list[2])
+
+    if month == 11 and day <= 22:
+        print('Sagittarius')
+    elif month == 12 and day <=21:
+        print('Sagittarius')
+
+    if month == 12 and day <= 22:
+        print('Capricorn')
+    elif month == 0o1 and day <=19:
+        print('Capricorn')
+
+    if month == 0o1 and day <=20:
+        print('Aquarius')
+    elif month == 0o2 and day <= 18:
+        print('Aquarius')
+
+    if month == 0o2 and day <= 19:
+        print('Pisces')
+    elif month == 0o3 and day <= 20:
+        print('Pisces')
+
+    if month == 0o3 and day <= 21:
+        print('Aries')
+    elif month == 0o4 and day <=19:
+        print('Aries')
+
+    if month == 0o4 and day <=20:
+        print('Taurus')
+    elif month == 0o5 and day <=20:
+        print('Taurus')
+
+    if month == 0o5 and day <=21:
+        print('Gemini')
+    elif month == 0o6 and day <=20:
+        print('Gemini')
+
+    if month == 0o6 and day <=21:
+        print('Cancer')
+    elif month == 0o7 and day <=22:
+        print('Cancer')
+
+    if month == 0o7 and day <=23:
+        print('Leo')
+    elif month == 0o8 and day <=22:
+        print('Leo')
+
+
+                
+    print(f"month={month} and day={day}")
+    return render(request, "horoscope_app/index.html")
 
 # def detail_two(request):
     
