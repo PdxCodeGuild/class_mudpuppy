ee61518d7e0bd734b3167949e14f62849777e917
haoua <haouangoyed@gmail.com>
2020-07-08 21:13:23 -0700

    push

--- Assignments/Haoua/Capstone/main_app/models.py
+++ Assignments/Haoua/Capstone/main_app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    # on_delete = if user deletes account, posts are deleted too
+    title = models.CharField(max_length=140)
+    content = models.TextField()
+    date = models.DateTimeField(default=timezone.now())
+
+    # engagement and reactions
+    likes = models.IntegerField()
+    dislikes = models.IntegerField()
+    fist = models.IntegerField()
+    haha = models.IntegerField()
+    angry = models.IntegerField()
+    
+
+
+
 
 # Create your models here.
