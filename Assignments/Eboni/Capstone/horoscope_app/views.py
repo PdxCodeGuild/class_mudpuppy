06a58c919df1a61d5420d4d10505d1204035f527
Eboni Washington <eboni.washington822@gmail.com>
2020-07-09 20:53:15 -0700

    adding if statements for signs

--- Assignments/Eboni/Capstone/horoscope_app/views.py
+++ Assignments/Eboni/Capstone/horoscope_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime, date
 from .secrets import api_key, api_id
+from .models import Sign, Birthday
 
 def index(request):
     if request.method == "POST": 
@@ -16,50 +17,72 @@ def detail(request):
     month = int(date_list[1])
     day = int(date_list[2])
 
+    date_object = datetime.strptime(date, "%Y-%m-%d")
+    # print(date_object)
+
     if month == 11 and day <= 22:
-        print('Sagittarius')
+       sign = 'Sagittarius'
     elif month == 12 and day <=21:
-        print('Sagittarius')
+        sign = 'Sagittarius'
 
     if month == 12 and day <= 22:
-        print('Capricorn')
-    elif month == 0o1 and day <=19:
-        print('Capricorn')
+        sign = 'Capricorn'
+    elif month == 1 and day <=19:
+        sign = 'Capricorn'
 
-    if month == 0o1 and day <=20:
-        print('Aquarius')
-    elif month == 0o2 and day <= 18:
-        print('Aquarius')
+    if month == 1 and day <=20:
+        sign = 'Aquarius'
+    elif month == 2 and day <= 18:
+        sign = 'Aquarius'
 
-    if month == 0o2 and day <= 19:
-        print('Pisces')
-    elif month == 0o3 and day <= 20:
-        print('Pisces')
+    if month == 2 and day <= 19:
+        sign = 'Pisces'
+    elif month == 3 and day <= 20:
+        sign = 'Pisces'
 
-    if month == 0o3 and day <= 21:
-        print('Aries')
-    elif month == 0o4 and day <=19:
-        print('Aries')
+    if month == 3 and day <= 21:
+        sign = 'Aries'
+    elif month == 4 and day <=19:
+        sign = 'Aries'
 
-    if month == 0o4 and day <=20:
-        print('Taurus')
-    elif month == 0o5 and day <=20:
-        print('Taurus')
+    if month == 4 and day <=20:
+        sign = 'Taurus'
+    elif month == 5 and day <=20:
+        sign = 'Taurus'
 
-    if month == 0o5 and day <=21:
-        print('Gemini')
-    elif month == 0o6 and day <=20:
-        print('Gemini')
+    if month == 5 and day <=21:
+        sign = 'Gemini'
+    # if month == 6
+    #     if day <=20: 
+    #         sign = 'Gemini'
+    #     elif day >=21:
+    #         sign = 'Cancer'
+        
+    if month == 6 and day <=21:
+        sign = 'Cancer'
+    elif month == 7 and day <=22:
+        sign = 'Cancer'
 
-    if month == 0o6 and day <=21:
-        print('Cancer')
-    elif month == 0o7 and day <=22:
-        print('Cancer')
+    if month == 7 and day <=23:
+        sign = 'Leo'
+    elif month == 8 and day <=22:
+        sign = 'Leo'
 
-    if month == 0o7 and day <=23:
-        print('Leo')
-    elif month == 0o8 and day <=22:
-        print('Leo')
+    if month == 8 and day <=23:
+        sign = 'Virgo'
+    elif month == 9 and day <=22:
+        sign = 'Virgo'
+
+    if month == 9 and day <=23:
+        sign = 'Libra'
+    elif month == 10 and day <=22:
+        sign = 'Libra'
+
+    user_sign = Sign.objects.get(sign=sign)
+    user_birthday = Birthday(date=date_object, sign=user_sign)
+    user_birthday.save()
+
+    
 
 
                 
