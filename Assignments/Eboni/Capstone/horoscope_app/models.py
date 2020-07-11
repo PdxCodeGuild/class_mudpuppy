0ec681cf8a58131e8badfe9776ac58bbe9730394
Eboni Washington <eboni.washington822@gmail.com>
2020-07-10 21:31:22 -0700

    adding some changes to the database messing with times

--- Assignments/Eboni/Capstone/horoscope_app/models.py
+++ Assignments/Eboni/Capstone/horoscope_app/models.py
@@ -2,6 +2,12 @@ from django.db import models
 
 class Sign(models.Model):
     sign = models.CharField(max_length= 20)
+    lower_date = models.DateTimeField()
+    upper_date = models.DateTimeField()
+
+    def check_date(self, in_date):
+        return in_date >= self.lower_date and in_date <= self.upper_date
+
     def __str__(self):
         return self.sign
     
@@ -9,6 +15,8 @@ class Sign(models.Model):
 class Birthday(models.Model):
     date = models.DateField(null=True, blank=True)
     sign = models.ForeignKey(Sign, null=True, on_delete=models.PROTECT)
+    def __str__(self):
+        return f"{str(self.date)} {self.sign}"
 
     
 
