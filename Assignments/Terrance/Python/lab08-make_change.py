fc224052a2c3488e1af41064a7a4bb3b3d74fc97
Terrance Mitchell <terrance.mitchell@gmail.com>
2020-07-20 20:32:59 -0700

    add files

--- Assignments/Terrance/Python/lab08-make_change.py
+++ Assignments/Terrance/Python/lab08-make_change.py
@@ -1,6 +1,6 @@
 #lab08-makechange.py
 
-print("I have a change converter. How many pennies do you have? ")
+print("I have a change converter. How many pennies do you have?")
 change_amount = int(input(': '))
 # print(f"That would equal {change_amount // 25} quarters and you would have {change_amount % 25} pennies leftover.")
 print(f"That would equal {change_amount // 25} quarters.") 
@@ -8,7 +8,11 @@ print(f"That would equal {change_amount // 25} quarters.")
 leftover_pennies = change_amount % 25
 
 # change_amount = int(input(': '))
-print(f"And, {leftover_pennies // 10} dimes leftover.") #{change_amount % 10} pennies leftover.")
+print(f"and, {leftover_pennies // 10} dimes leftover.") #{change_amount % 10} pennies leftover.")
+leftover_pennies = leftover_pennies % 10
 
-change_amount = int(input(': '))
-print(f"And, {change_amount // 5} nickels leftover.") #{change_amount % 5} pennies leftover.")
+# change_amount = int(input(': '))
+print(f"and, {leftover_pennies // 5} nickels leftover.") #{change_amount % 5} pennies leftover.")
+leftover_pennies = leftover_pennies % 5
+
+print(f"and, {leftover_pennies // 1} pennies leftover.")
